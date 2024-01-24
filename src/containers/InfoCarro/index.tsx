ed668e29437e10bbe55257957d4f6175b9a0f07d
Tallyson Freitas <tallyson.eu@outlook.com>
2024-01-24 18:58:12 -0300

    correcao comportamento ContainerFormInteresse

--- src/containers/InfoCarro/index.tsx
+++ src/containers/InfoCarro/index.tsx
@@ -1,8 +1,14 @@
 import {
   CapTitulo,
+  CondicoesVeiculo,
   ContainerFormInteresse,
   ContainerInfoCarro,
-  InformacoesCarro
+  IconeInfo,
+  InformacoesCarro,
+  StatusPlus,
+  TextoForte,
+  TextoInfo,
+  TituloSecundario
 } from './styles'
 
 const InfoCarro = () => {
@@ -14,45 +20,80 @@ const InfoCarro = () => {
             FORD <span>ECOSPORT</span>
             <span>2.0 FREESTYLE PLUS 16V FLEX 4P POWERSHIFT</span>
           </h1>
-          <div className="localidade">
-            <h2>Cidade</h2>
-            <strong>São Bernado do Campo - SP</strong>
+          <div>
+            <TituloSecundario>Cidade</TituloSecundario>
+            <TextoForte>São Bernado do Campo - SP</TextoForte>
           </div>
           <ul className="lista-info">
             <li>
-              <h2>Ano</h2>
-              <strong>2015/2015</strong>
+              <TituloSecundario>Ano</TituloSecundario>
+              <TextoForte>2015/2015</TextoForte>
             </li>
             <li>
-              <h2>KM</h2>
-              <strong>94.679</strong>
+              <TituloSecundario>KM</TituloSecundario>
+              <TextoForte>94.679</TextoForte>
             </li>
             <li>
-              <h2>Câmbio</h2>
-              <strong>Semi-automático</strong>
+              <TituloSecundario>Câmbio</TituloSecundario>
+              <TextoForte>Semi-automático</TextoForte>
             </li>
             <li>
-              <h2>Carroceria</h2>
-              <strong>Utilitário esportivo</strong>
+              <TituloSecundario>Carroceria</TituloSecundario>
+              <TextoForte>Utilitário esportivo</TextoForte>
             </li>
             <li>
-              <h2>Combustível</h2>
-              <strong>Gasolina e álcool</strong>
+              <TituloSecundario>Combustível</TituloSecundario>
+              <TextoForte>Gasolina e álcool</TextoForte>
             </li>
             <li>
-              <h2>FFinal de placa</h2>
-              <strong>8</strong>
+              <TituloSecundario>FFinal de placa</TituloSecundario>
+              <TextoForte>8</TextoForte>
             </li>
             <li>
-              <h2>Cor</h2>
-              <strong>Prata</strong>
+              <TituloSecundario>Cor</TituloSecundario>
+              <TextoForte>Prata</TextoForte>
             </li>
             <li>
-              <h2>Aceita troca</h2>
-              <strong>Sim</strong>
+              <TituloSecundario>Aceita troca</TituloSecundario>
+              <TextoForte>Sim</TextoForte>
             </li>
           </ul>
         </CapTitulo>
+        <CondicoesVeiculo>
+          <TituloSecundario>Sobre os diferenciais do anúncio</TituloSecundario>
+          <div className="videochamada">
+            <div>
+              <IconeInfo img="https://www.webmotors.com.br/assets/img/stamps/video-chamada.svg" />
+              <div className="container-texto">
+                <TextoForte>Videochamada</TextoForte>
+                <TextoInfo>
+                  Agenda um horário para ver o carro em vídeo
+                </TextoInfo>
+              </div>
+            </div>
+            <button>Agendar videochamada</button>
+          </div>
+          <StatusPlus>
+            <div className="status-header">
+              <IconeInfo img="https://www.webmotors.com.br/assets/img/stamps/super-preco.svg" />
+              <TextoForte>Super Preço</TextoForte>
+              <svg width="24" height="24" viewBox="0 0 24 24" fill="none">
+                <path
+                  fillRule="evenodd"
+                  clipRule="evenodd"
+                  d="M17.7072 10.2071L12.7073 15.2071C12.3163 15.5981 11.6842 15.5981 11.2933 15.2071L6.29325 10.2071C5.90225 9.81612 5.90225 9.18412 6.29325 8.79312C6.68425 8.40212 7.31625 8.40212 7.70725 8.79312L12.0002 13.0861L16.2933 8.79312C16.6842 8.40212 17.3162 8.40212 17.7072 8.79312C17.9022 8.98812 18.0002 9.24412 18.0002 9.50012C18.0002 9.75612 17.9022 10.0121 17.7072 10.2071Z"
+                  fill="#2E2D37"
+                ></path>
+              </svg>
+            </div>
+            <div className="dropdown-info">
+              <p>
+                Anúncio com preço <strong>abaixo da tabela FIPE</strong>.
+                Aproveite e faça um ótimo negócio!
+              </p>
+            </div>
+          </StatusPlus>
+        </CondicoesVeiculo>
       </InformacoesCarro>
       <ContainerFormInteresse>
         <h1>R$ 51.900</h1>
--- src/containers/InfoCarro/styles.ts
+++ src/containers/InfoCarro/styles.ts
@@ -1,5 +1,7 @@
 import styled from 'styled-components'
 
+export type BackgroundImage = { img: string }
+
 export const ContainerInfoCarro = styled.div`
   display: grid;
   padding: 0 50px;
@@ -27,9 +29,23 @@ export const ContainerFormInteresse = styled.div`
   z-index: 1;
   border-radius: 8px;
   position: sticky;
-  top: 0px;
+  top: 131px;
   box-shadow: 0 2px 9px 0 rgba(158, 184, 209, 0.41);
   padding-top: 80px;
+`
+
+export const TituloSecundario = styled.h2`
+  font-size: 12px;
+  color: #696977;
+  font-weight: 400;
+`
+
+export const TextoForte = styled.strong`
+  display: flex;
+  color: #2e2d37;
+  font-size: 16px;
+
+  font-weight: 600;
 `
 
 export const CapTitulo = styled.div`
@@ -51,42 +67,126 @@ export const CapTitulo = styled.div`
     letter-spacing: 0.07px;
   }
 
-  .localidade {
-    h2 {
-      font-size: 12px;
-      color: #696977;
-      font-weight: 400;
-    }
-
-    strong {
-      display: flex;
-      color: #2e2d37;
-      font-size: 16px;
-      padding: 0 50px 0 0;
-      font-weight: 600;
-    }
-  }
-
   .lista-info {
     display: grid;
     grid-template-columns: 1fr 1fr 1fr 1fr;
     row-gap: 16px;
 
-    li {
-      h2 {
-        font-size: 12px;
-        color: #696977;
-        font-weight: 400;
-      }
+    strong {
+      padding: 0 50px 0 0;
+    }
+  }
+`
 
-      strong {
-        display: flex;
-        color: #2e2d37;
-        font-size: 16px;
-        padding: 0 50px 0 0;
-        font-weight: 600;
-        padding-right: 56px;
+export const IconeInfo = styled.div<BackgroundImage>`
+  background-image: url(${(props) => props.img});
+  background-repeat: no-repeat;
+  background-size: 100%;
+  width: 25px;
+  height: 49px;
+`
+
+export const CondicoesVeiculo = styled.div`
+  padding: 36px 40px;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  max-height: 300px;
+  overflow-y: visible;
+  gap: 8px;
+  border-bottom: 1px solid #dedede;
+
+  .videochamada {
+    display: flex;
+    justify-content: space-between;
+    aling-items: center;
+    padding: 16px;
+
+    & > div {
+      display: flex;
+    }
+
+    .container-texto {
+      margin: 0px 16px;
+    }
+    button {
+      color: rgb(255, 255, 255);
+      padding: 10px 16px;
+      border-radius: 8px;
+      background-color: rgb(243, 18, 60);
+      display: flex;
+      -webkit-box-align: center;
+      align-items: center;
+      font-weight: bold;
+      font-size: 12px;
+      line-height: 150%;
+      transition: background-color 0.3s ease 0s;
+      cursor: pointer;
+      border: none;
+      margin: 12px 0px;
+
+      &:hover {
+        background-color: rgb(194, 14, 48);
+        transition: background-color 0.3s ease 0s;
       }
     }
   }
+`
+
+export const StatusPlus = styled.div`
+  max-width: 240px;
+  width: 100%;
+  overflow-y: hidden;
+  max-height: 64px;
+  border-radius: 8px;
+
+  transition:
+    max-height 0.7s ease-in 0s,
+    opacity 0.7s ease-in 0s,
+    box-shadow 0.7s ease-in 0s;
+
+  .status-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px 24px;
+    position: relative;
+    cursor: pointer;
+  }
+
+  .dropdown-info {
+    display: block;
+    height: 0px;
+    transition:
+      padding 0.7s ease-in 0s,
+      height 0.7s ease-in 0s;
+
+    p {
+      color: rgb(45, 45, 55);
+      line-height: 150%;
+      font-weight: normal;
+      display: inline;
+      font-size: 14px;
+
+      span {
+        font-weight: 600;
+      }
+    }
+  }
+
+  &:hover {
+    max-height: 240px;
+    box-shadow: 0px 16px 16px rgba(0, 0, 0, 0.3);
+  }
+
+  &:hover .dropdown-info {
+    padding: 16px;
+    height: 100%;
+  }
+`
+
+export const TextoInfo = styled.p`
+  font-size: 16px;
+  color: #2e2d37;
+  line-height: 24px;
 `

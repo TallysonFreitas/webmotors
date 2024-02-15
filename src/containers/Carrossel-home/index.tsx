4d2fc2afe18cecdf03f755eee1ebf910c54c68c0
Tallyson Freitas <tallyson.eu@outlook.com>
2024-02-15 09:52:05 -0300

    feat: economizando dados com recurso picture

--- src/containers/Carrossel-home/index.tsx
+++ src/containers/Carrossel-home/index.tsx
@@ -1,39 +1,63 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
-import { ImageCarrossel } from './style'
+import { ImageCarrossel, SourceCarrossel } from './style'
 
 const CarrosselHome = () => {
   return (
     <Swiper className="" slidesPerView={1} navigation autoplay loop>
       <SwiperSlide>
         <a href="https://www.webmotors.com.br/carros/estoque?lkid=2183">
-          <ImageCarrossel
-            src="https://tpc.googlesyndication.com/simgad/7434839780464587051?"
-            alt=""
-          />
+          <picture>
+            <SourceCarrossel
+              srcSet="https://tpc.googlesyndication.com/simgad/7434839780464587051?"
+              media="(min-width: 641px)"
+            />
+            <ImageCarrossel
+              src="https://tpc.googlesyndication.com/simgad/1853439400975895390?"
+              alt=""
+            />
+          </picture>
         </a>
       </SwiperSlide>
       <SwiperSlide>
         <a href="https://www.webmotors.com.br/financiamento/?lkid=1691">
-          <ImageCarrossel
-            src="https://tpc.googlesyndication.com/simgad/7444578278502576569?"
-            alt=""
-          />
+          <picture>
+            <SourceCarrossel
+              srcSet="https://tpc.googlesyndication.com/simgad/7434839780464587051?"
+              media="(min-width: 641px)"
+            />
+            <ImageCarrossel
+              src="https://tpc.googlesyndication.com/simgad/1853439400975895390?"
+              alt=""
+            />
+          </picture>
         </a>
       </SwiperSlide>
       <SwiperSlide>
         <a href="https://ajuda.webmotors.com.br/hc/pt-br/articles/360038764071-Como-fa%C3%A7o-para-evitar-golpes-ao-vender-">
-          <ImageCarrossel
-            src="https://tpc.googlesyndication.com/simgad/10205041719707627470?"
-            alt=""
-          />
+          <picture>
+            <SourceCarrossel
+              srcSet="https://tpc.googlesyndication.com/simgad/7434839780464587051?"
+              media="(min-width: 641px)"
+            />
+            <ImageCarrossel
+              src="https://tpc.googlesyndication.com/simgad/1853439400975895390?"
+              alt=""
+            />
+          </picture>
         </a>
       </SwiperSlide>
       <SwiperSlide>
         <a href="https://www.webmotors.com.br/carros/estoque?lkid=2054">
-          <ImageCarrossel
-            src="https://tpc.googlesyndication.com/simgad/18327656902530545789?"
-            alt=""
-          />
+          <picture>
+            <SourceCarrossel
+              srcSet="https://tpc.googlesyndication.com/simgad/7434839780464587051?"
+              media="(min-width: 641px)"
+            />
+            <ImageCarrossel
+              src="https://tpc.googlesyndication.com/simgad/1853439400975895390?"
+              alt=""
+            />
+          </picture>
         </a>
       </SwiperSlide>
     </Swiper>

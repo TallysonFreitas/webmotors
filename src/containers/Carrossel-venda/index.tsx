62a92d02ece2a6c3198a0ac63c4879c870bca8e3
Tallyson Freitas <tallyson.eu@outlook.com>
2024-02-16 16:27:11 -0300

    feat: adicionando troca de quantidade de carrossel apartir da responsividade

--- src/containers/Carrossel-venda/index.tsx
+++ src/containers/Carrossel-venda/index.tsx
@@ -1,9 +1,20 @@
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { ImageCarrossel, ContainerImagemCarrossel } from './style'
+import { useEffect, useState } from 'react'
 
 const CarrosselVenda = ({ imgs }: { imgs: string[] }) => {
+  const [imagensNoCarrossel, setImagensNoCarrossel] = useState(1)
+
+  useEffect(() => {
+    if (window.screen.width >= 640) {
+      setImagensNoCarrossel(3)
+    } else {
+      setImagensNoCarrossel(1)
+    }
+  }, [])
+
   return (
-    <Swiper slidesPerView={3} navigation loop>
+    <Swiper slidesPerView={imagensNoCarrossel} navigation loop>
       {imgs.map((each) => (
         <SwiperSlide key="1">
           <ContainerImagemCarrossel>

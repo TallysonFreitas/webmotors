4261903e4d4991193975b770d39d73a31e08b9b4
Tallyson Freitas <tallyson.eu@outlook.com>
2024-02-23 14:07:49 -0300

    feat: adiciona reducer Favorito

---
+++ src/redux/reducers/Favorito.ts
@@ -0,0 +1,29 @@
+import { PayloadAction, createSlice } from '@reduxjs/toolkit'
+
+type FavoritosType = {
+  favs: string[]
+}
+
+const initialState: FavoritosType = {
+  favs: []
+}
+
+const FavoritosSlice = createSlice({
+  name: 'Favoritos',
+  initialState,
+  reducers: {
+    adicionaFavorito: (state, action: PayloadAction<string>) => {
+      if (state.favs.findIndex((each) => each === action.payload) === -1) {
+        state.favs = [...state.favs, action.payload]
+      }
+    },
+    removeFavorito: (state, action: PayloadAction<string>) => {
+      state.favs = state.favs.filter(
+        (each) => each.toLowerCase() !== action.payload.toLowerCase()
+      )
+    }
+  }
+})
+
+export default FavoritosSlice.reducer
+export const { adicionaFavorito, removeFavorito } = FavoritosSlice.actions

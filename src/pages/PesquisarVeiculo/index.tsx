072ca42190635519c2dbaa7be4c412e06a0db0ac
Tallyson Freitas <tallyson.eu@outlook.com>
2024-02-26 10:52:28 -0300

    fix: centralizando controle de informacoes no componente mae

--- src/pages/PesquisarVeiculo/index.tsx
+++ src/pages/PesquisarVeiculo/index.tsx
@@ -1,15 +1,95 @@
+import { useSelector } from 'react-redux'
 import CarrosBuscados from '../../containers/CarrosBuscados'
-import Footer from '../../containers/Footer'
 import Header from '../../containers/Header'
 import { EstiloGlobal } from '../../style'
+import { useEffect, useState } from 'react'
+import { RootReducer } from '../../redux'
+import Loading from '../../containers/Loading'
+
+const exemploRetorno = {
+  id: 100010,
+  modelo: 'Model S',
+  marca: 'Tesla',
+  valorAnuncio: 949990,
+  informacoes: {
+    cidadeCapital: 'Piracicaba - SP',
+    ano: '2021/2021',
+    KM: '1.600',
+    cambio: 'Automático',
+    carroceria: 'Sedã',
+    combustível: 'Gasolina e elétrico',
+    finalDePlaca: 0,
+    cor: 'Cinza',
+    aceitaTroca: 'Sim'
+  },
+  sobreVeiculo: '',
+  imgs: [''],
+  PromoCondicoes: { carDelivery: true, troco: true, superPreco: true },
+  itensVeiculo: [''],
+  loja: {
+    nome: 'Boca Mafra Premium',
+    localizacao: 'Balneário Camboriú, SC'
+  },
+  link: '',
+  comparaPrecos: { mediaWebMotors: 1016626, fipe: 0 }
+}
+type CarrosFiltradosType = typeof exemploRetorno
 
 const PesquisarVeiculo = () => {
+  const { termo } = useSelector((state: RootReducer) => state.buscarFiltro)
+
+  const [carregando, setCarregando] = useState(true)
+
+  const [carrosCarregados, setCarrosCarregados] = useState<
+    CarrosFiltradosType[]
+  >([])
+
+  async function carregaVeiculos() {
+    await fetch('./cars.json')
+      .then((data) => data.json())
+      .then((response) => {
+        setCarrosCarregados(response),
+          setCarrosFiltrados(response),
+          setInterval(() => {
+            setCarregando(false)
+          }, 1000)
+      })
+  }
+
+  const [carrosFiltrados, setCarrosFiltrados] = useState<CarrosFiltradosType[]>(
+    []
+  )
+
+  // atualiza busca de carros
+  useEffect(() => {
+    if (termo == '') {
+      setCarrosFiltrados(carrosCarregados)
+    } else {
+      setCarrosFiltrados(
+        carrosCarregados.filter(
+          (each) =>
+            `${each.marca} ${each.modelo}`
+              .toLowerCase()
+              .includes(termo.toLowerCase()) == true
+        )
+      )
+    }
+  }, [termo])
+
+  // ao carregar a pagina executa Promisse para carregar veiculo
+  useEffect(() => {
+    carregaVeiculos()
+  }, [])
+
   return (
     <>
       <EstiloGlobal />
       <Header />
-      <CarrosBuscados />
-      <Footer />
+      {carregando === true ? (
+        <Loading />
+      ) : (
+        <CarrosBuscados array={carrosFiltrados} />
+      )}
     </>
   )
 }
